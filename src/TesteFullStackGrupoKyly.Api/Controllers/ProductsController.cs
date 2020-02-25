37cc48d33047280d6c4723630a6fb989e38a00d0
Maicon Gabriel Friedel <maicon.friedel@kyly.com.br>
2020-02-25 13:07:18 -0300

    Criado estrutura inicial da API Rest

---
+++ src/TesteFullStackGrupoKyly.Api/Controllers/ProductsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TesteFullStackGrupoKyly.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    }
+}
